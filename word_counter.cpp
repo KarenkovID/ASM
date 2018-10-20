a64241d93ee978c25fa6f36777bd51b73ddf9c8f
Karenkov Igor <karenkovigor@gmail.com>
2018-10-20 12:16:39 +0300

    fix

--- word_counter.cpp
+++ word_counter.cpp
@@ -1,13 +1,15 @@
-#include <bits/stdc++.h>
 #include <stdint.h>
 #include <xmmintrin.h>
 #include <tmmintrin.h>
+#include <string>
+#include <iostream>
 
 using namespace std;
 
 const int BLOCK_SIZE = 16;
+const bool testPerformance = true;
 
-size_t linearly(string s, size_t n) {
+inline size_t greed(string s, size_t n) {
     size_t ans = 0;
     for (size_t i = 0; i < n; i++) {
         if (s[i] != ' ' && (s[i - 1] == ' ' || i == 0)) {
@@ -17,7 +19,7 @@ size_t linearly(string s, size_t n) {
     return ans;
 }
 
-void recount_flush(size_t &res, __m128i &a) {
+inline void flush(size_t &res, __m128i &a) {
     for (int i = 0; i < BLOCK_SIZE; i++) {
         res += (int) *((uint8_t *) &a + i);
     }
@@ -25,11 +27,12 @@ void recount_flush(size_t &res, __m128i &a) {
 }
 
 size_t count(const char *str, size_t size) {
-    const __m128i MASK_SPACES = _mm_set_epi8(' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
-                                             ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
-    const __m128i MASK_ONES = _mm_set_epi8(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+    static const __m128i MASK_SPACES = _mm_set_epi8(' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+    static const __m128i MASK_ONES = _mm_set_epi8(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+
     size_t res = 0;
     size_t offset = 0;
+
     if (*str != ' ') {
         ++res;
     }
@@ -37,8 +40,9 @@ size_t count(const char *str, size_t size) {
     //aligning
     bool was_space = false;
     for (; (size_t) (str + offset) % BLOCK_SIZE != 0; offset++) {
-        res += (was_space && str[offset] != ' ');
-        was_space = (str[offset] == ' ');
+        bool isSpace = str[offset] == ' ';
+        res += (was_space && !isSpace);
+        was_space = isSpace;
     }
 
     if (was_space && offset != 0 && *(str + offset) != ' ') {
@@ -47,30 +51,31 @@ size_t count(const char *str, size_t size) {
 
     __m128i ans = _mm_setzero_si128();
     size_t n = size - (size - offset) % BLOCK_SIZE - BLOCK_SIZE;
-    __m128i prev, curr = _mm_cmpeq_epi8(_mm_load_si128((__m128i *) (str + offset)), MASK_SPACES);
+    __m128i curSpaces = _mm_cmpeq_epi8(_mm_load_si128((__m128i *) (str + offset)), MASK_SPACES);
+    __m128i nextSpaces;
     int k = 0;
+
+    //assembly magic (no)
     for (; offset < n; offset += BLOCK_SIZE) {
-        prev = curr;
-        curr = _mm_cmpeq_epi8(_mm_load_si128((__m128i *) (str + offset + BLOCK_SIZE)), MASK_SPACES);
-        __m128i shifted_spaces = _mm_alignr_epi8(curr, prev, 1);
-        __m128i count = _mm_and_si128(_mm_andnot_si128(shifted_spaces, prev), MASK_ONES);
+        nextSpaces = _mm_cmpeq_epi8(_mm_load_si128((__m128i *) (str + offset + BLOCK_SIZE)), MASK_SPACES);
+        //https://msdn.microsoft.com/en-us/library/bb514041(v=vs.120).aspx
+        __m128i shiftedCurSpaces = _mm_alignr_epi8(nextSpaces, curSpaces, 1);
+        __m128i count = _mm_and_si128(_mm_andnot_si128(shiftedCurSpaces, curSpaces), MASK_ONES);
         ans = _mm_add_epi8(ans, count);
+
         ++k;
-        //protection from overflow
         if (k == 255) {
-            recount_flush(res, ans);
+            flush(res, ans);
             k = 0;
         }
+        curSpaces = nextSpaces;
     }
 
-    recount_flush(res, ans);
-
+    flush(res, ans);
     offset = n;
-
     if (*(str + offset - 1) == ' ' && *(str + offset) != ' ') {
         --res;
     }
-
     was_space = *(str + offset - 1) == ' ';
     for (size_t i = offset; i < size; i++) {
         char cur = *(str + i);
@@ -83,7 +88,7 @@ size_t count(const char *str, size_t size) {
 
 string test() {
     std::string s = "";
-    int len = (rand() % 100000g);
+    int len = (rand() % 100000);
     for (int i = 0; i < len; ++i) {
         int k = rand() % 2;
         if (k) {
@@ -101,7 +106,7 @@ int main() {
         std::string s = test();
         const char *st = s.c_str();
         cout << s << "\n";
-        size_t right_ans = linearly(s, (int) s.size());
+        size_t right_ans = greed(s, (int) s.size());
         size_t my_ans = count(st, s.size());
         cout << "right: " << right_ans << "\n";
         cout << "my: " << my_ans << "\n";
